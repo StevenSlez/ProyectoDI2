d7cc1132725c5af61ceb3c134cb6cdee5c6e9ee7
Steven <slezak87@gmail.com>
2024-01-11 20:19:43 +0100

    Fin 1.0

--- ProyectoDI2/MainWindow.axaml.cs
+++ ProyectoDI2/MainWindow.axaml.cs
@@ -16,6 +16,7 @@ public partial class MainWindow : Window
     int num = 0;
     String ruta = "";
     OpenFileDialog ofd;
+    string rutaData = "holocron.data";
     public MainWindow()
     {
         InitializeComponent();
@@ -160,7 +161,9 @@ public partial class MainWindow : Window
         }
         catch (Exception ex)
         {
-            //MessageBox.Show("Error al mostrar la imagen: " + ex.Message);
+            PanelMensaje.IsVisible = true;
+            ErrorText.FontSize = 11;
+            ErrorText.Text = "Error al mostrar la imagen: \n" + ex.Message;
         }
     }
     
@@ -188,7 +191,9 @@ public partial class MainWindow : Window
             if (!imp.Equals("Si") && !imp.Equals("si") && !imp.Equals("No") && !imp.Equals("no")) //en caso de que se escriba otra cosa que no sea 'si' o 'no'
             {
                 imp = "Si";
-                //MessageBox.Show("No sabes escribir... escoria rebelde.\nAsumiremos que el planeta ES imperialista", "Error en campo Imperialista", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                PanelMensaje.IsVisible = true;
+                ErrorText.FontSize = 11;
+                ErrorText.Text = "No sabes escribir... escoria rebelde.\nAsumiremos que el planeta ES \nimperialista";
             }
 
             if (imp.Equals("Si") || imp.Equals("si"))
@@ -217,7 +222,6 @@ public partial class MainWindow : Window
             {
                 ruta = result[0]; // Obtiene la ruta del archivo seleccionado
                 Console.WriteLine(ruta);
-                //pbTik.Source = new Bitmap(ruta); // Muestra la imagen en el Image control
                 pbTik.IsVisible = true; // Hace visible el control de la imagen
             }
             catch (Exception ex)
@@ -239,24 +243,29 @@ public partial class MainWindow : Window
 
         String nombre = txtNombreNew.Text;
         String Shabitantes = txtHabNew.Text;
-        Shabitantes = Shabitantes.Replace('.', ',');
 
         if (!string.IsNullOrEmpty(Shabitantes))
+        {
+            Shabitantes = Shabitantes.Replace('.', ',');
             habitantes = float.Parse(Shabitantes);
-        else
+        }
+        else // si no se pone nada se entiende que tiene 0 habitantes
             habitantes = 0;
 
         String imperialista = txtImpNew.Text;
-        if (!imperialista.Equals("Si") && !imperialista.Equals("si") && !imperialista.Equals("No") && !imperialista.Equals("no")) //en caso de que se escriba otra cosa que no sea 'si' o 'no'
+        if (!string.IsNullOrEmpty(imperialista) && (!imperialista.Equals("Si") || !imperialista.Equals("si") || !imperialista.Equals("No") || !imperialista.Equals("no"))) //en caso de que se escriba otra cosa que no sea 'si' o 'no'
         {
             imperialista = "Si";
-            //MessageBox.Show("No sabes escribir... escoria rebelde.\nAsumiremos que el planeta ES imperialista", "Error en campo Imperialista", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            PanelMensaje.IsVisible = true;
+            ErrorText.FontSize = 11;
+            ErrorText.Text = "No sabes escribir... escoria rebelde.\nAsumiremos que el planeta ES \nimperialista";
         }
 
         //si alguno de los parametros se queda vacio o no se ha elegido png, salta el aviso en una ventana emergente
-        if (pbTik.IsVisible.Equals(false) || string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtHabitantes.Text) || string.IsNullOrEmpty(txtImperialista.Text))
+        if (!pbTik.IsVisible || string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtHabitantes.Text) || string.IsNullOrEmpty(txtImperialista.Text))
         {
-            //MessageBox.Show("Joven padawan, no puedes dejar ningún campo vacío.\n(Ni la imagen sin seleccionar)", "Campos Vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            PanelMensaje.IsVisible = true;
+            ErrorText.Text = "Joven padawan, no puedes dejar ningún campo vacío.\n(Ni la imagen sin seleccionar)";
         }
         else //si están todos los datos correctos, creo el planeta nuevo
         {
@@ -278,5 +287,100 @@ public partial class MainWindow : Window
             //al registrar un planeta nuevo, vuelvo a llamar al metodo mostrarDatos para actualizar
             mostrarDatos();
         }
+    }
+    
+    private void btnGuardar(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(rutaData))
+            {
+                foreach (var data in planetas)
+                {
+                    writer.WriteLine(data.ToString());
+                }
+            }
+
+            PanelMensaje.IsVisible = true;
+            ErrorText.Text = "Joven padawan, se ha guardado \ncorrectamente los datos \nen el Holocrón.";
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Error al guardar los datos: " + ex.Message);
+        }
+    }
+
+    private void btnCargar(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(rutaData))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Planeta obj = ParsePlanetaFromString(line); //uso el metodo creado más adelante
+
+                    if (obj != null)
+                    {
+                        planetas.Add(obj); // agrega el Planeta a la lista
+                    }
+                }
+
+                PanelMensaje.IsVisible = true;
+                ErrorText.Text = "Datos cargados correctamente \ndesde el Holocrón. \nQue no te lo quite los Sith!";
+                
+                mostrarDatos(); //actualizo los datos
+            }
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Error al leer los datos: " + ex.Message);
+        }
+    }
+    
+    private Planeta ParsePlanetaFromString(string line)
+    {
+        string[] partes = line.Split('-'); //separa los elementos por cada '-' (al principio era por cada ',' pero generaba confusion a la hora de guardar el float decimal con su propia ',')
+
+        // verifica si hay el número de elementos necesarios para crear un Planeta válido
+        if (partes.Length >= 5)
+        {
+            string nombre = partes[1];
+            float habitantes;
+            float.TryParse(partes[2], out habitantes);
+            bool imperialista = partes[3].Equals("True", StringComparison.OrdinalIgnoreCase);
+
+            byte[] imagen;
+
+            try
+            {
+                imagen = Convert.FromBase64String(partes[4]);
+            }
+            catch (FormatException ex)
+            {
+                // en caso de que el contenido no sea válido en formato Base64
+                Console.WriteLine("Error al convertir la imagen de Base64 a bytes: " + ex.Message);
+                return null;
+            }
+
+            Planeta planeta = new Planeta(nombre, habitantes, imperialista, imagen);
+
+            mostrarDatos();
+            lblTotal.Text = "" + (planetas.Count + 1); // le tengo que hacer esto porque al cargar mostrarDatos() me lo pone con 1 de menos...
+
+            return planeta;
+        }
+        else
+        {
+            // si la línea no tiene los elementos suficientes para un Planeta, se devuelve null
+            return null;
+        }
+    }
+    
+    private async void btnFatalError(object? sender, RoutedEventArgs routedEventArgs) //metodo que hace desaparecer el Panel del mensaje de error
+    {
+        PanelMensaje.IsVisible = false;
     }
 }
